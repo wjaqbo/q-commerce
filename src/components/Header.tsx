5393271c72280cfecfaa712e9eb686b6b2693566
Wiktor Jakubowski <qbagency@gmail.com>
2023-09-29 13:39:23 -0500

    Add shadcn/ui, dark mode, prettier-plugin-tailwind

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,19 @@
-import Link from 'next/link';
-import React from 'react';
-import { UserButton } from '@clerk/nextjs';
+import Link from "next/link"
+import React from "react"
+import { UserButton } from "@clerk/nextjs"
+import { Button } from "@/components/ui/button"
+import { ModeToggle } from "@/components/mode-toggle"
 
 export default function Header() {
   return (
-    <header className="fixed left-0 top-0 flex gap-4 w-full justify-center items-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+    <header className="fixed left-0 top-0 flex w-full items-center justify-center gap-4 border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
       <Link href="/">Home</Link>
       <Link href="/dashboard">Dashboard</Link>
+      <Button variant="outline">click me</Button>
+      <ModeToggle />
       <div className="ml-auto">
         <UserButton afterSignOutUrl="/" />
       </div>
     </header>
-  );
+  )
 }
